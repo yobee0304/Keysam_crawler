99974bba3a9afcf11f0d6934a4a4b9a2c211cc18
안찬영 <an20171@naver.com>
2020-07-29 15:13:53 +0900

    create project

--- Keysam_crawling_web.py
+++ Keysam_crawling_web.py
@@ -1,3 +1,15 @@
+import datetime
+
+def collect_article():
+
+    # 임시 테스트
+    url = "https://recruit.webtoonscorp.com/webtoon/ko/job/list?classNm=developer"용
+
+    
 
 if __name__ == "__main__":
-    print("hello")
+    print("Start Crawling... :", datetime.datetime.now())
+
+    collect_article()
+
+    print("End Crawling... :", datetime.datetime.now())
